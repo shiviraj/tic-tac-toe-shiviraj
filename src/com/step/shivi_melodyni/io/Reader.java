7119f2bc4831c3d6b122621ab9ff52867c7430ef
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-11-25 18:35:37 +0530

    added Reader and Write interface to do io operations

---
+++ src/com/step/shivi_melodyni/io/Reader.java
@@ -0,0 +1,5 @@
+package com.step.shivi_melodyni.io;
+
+public interface Reader {
+    int readInt();
+}
---
+++ src/com/step/shivi_melodyni/io/Writer.java
@@ -0,0 +1,5 @@
+package com.step.shivi_melodyni.io;
+
+public interface Writer {
+    void write(String string);
+}
