1abd376d10effec377c3c9ca92d21d5858bc9abc
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-11-26 16:32:40 +0530

    added a static Inner Class Move in Board

--- src/com/step/shivi_melodyni/game/Board.java
+++ src/com/step/shivi_melodyni/game/Board.java
@@ -3,6 +3,9 @@ package com.step.shivi_melodyni.game;
 import com.step.shivi_melodyni.dto.BoardDTO;
 
 public class Board {
+    static class Move{
+        int row, col;
+    }
 
     private final char[][] cells;
 
@@ -18,18 +21,23 @@ public class Board {
         return new BoardDTO(this.cells);
     }
 
+    private Move findRowColNo(int cellNo){
+        Move move = new Move();
+        move.row = (cellNo - 1) / this.cells.length;
+        move.col = (cellNo - 1) % this.cells.length;
+        return move;
+    }
+
     public void place(int cellNo, char symbol) {
-        int rowNo = (cellNo - 1) / this.cells.length;
-        int colNo = (cellNo - 1) % this.cells.length;
-        this.cells[rowNo][colNo] = symbol;
+        Move move = findRowColNo(cellNo);
+        this.cells[move.row][move.col] = symbol;
     }
 
     public boolean isValidMove(int cellNo) {
         if(cellNo < 1 || cellNo > this.size()){
             return false;
         }
-        int rowNo = (cellNo - 1) / this.cells.length;
-        int colNo = (cellNo - 1) % this.cells.length;
-        return this.cells[rowNo][colNo] == '\u0000';
+        Move move = findRowColNo(cellNo);
+        return this.cells[move.row][move.col] == '\u0000';
     }
 }
