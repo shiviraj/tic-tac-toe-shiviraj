94206923e35a59f953ccc827c43d523e3ee1e98b
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-11-28 12:42:51 +0530

    handling arrayIndexOutOfBound exception while taking command line args

--- src/com/step/shivi_melodyni/TicTacToe.java
+++ src/com/step/shivi_melodyni/TicTacToe.java
@@ -7,9 +7,15 @@ import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        String player1 = args[0];
-        String player2 = args[1];
-
+        String player1;
+        String player2;
+        try {
+            player1 = args[0];
+            player2 = args[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please Enter Both Player's Name!\nEg:- java -jar jarfile Ramesh Suresh");
+            return;
+        }
         int boardSize = 3;
         Game game = new Game(player1, player2, boardSize);
         ConsolePresenter consolePresenter = new ConsolePresenter(System.out::print, new Scanner(System.in)::nextLine);
