057de61ee3403d16e421b1379cada94bc908521c
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-12-02 10:15:54 +0530

    Modified main in TicTacToe.

--- src/com/step/shivi_melodyni/ttt/TicTacToe.java
+++ src/com/step/shivi_melodyni/ttt/TicTacToe.java
@@ -3,6 +3,7 @@ package com.step.shivi_melodyni.ttt;
 import com.step.shivi_melodyni.ttt.ai.AIPlayer;
 import com.step.shivi_melodyni.ttt.model.Game;
 import com.step.shivi_melodyni.ttt.model.player.HumanPlayer;
+import com.step.shivi_melodyni.ttt.model.player.Player;
 import com.step.shivi_melodyni.ttt.presenter.ConsolePresenter;
 
 import java.util.Scanner;
@@ -10,18 +11,18 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
         int boardSize = 3;
-        String player1Name = "Human";
 
-        AIPlayer aiPlayer = new AIPlayer();
-        if (args.length > 0) {
-            player1Name = args[0];
-        }
-        HumanPlayer HumanPlayer = new HumanPlayer(player1Name, 'X');
-        Game game = new Game(HumanPlayer, aiPlayer, boardSize);
+        String playerName = (args.length > 0) ? args[0] : "Human";
+        Player player = new HumanPlayer(playerName, 'X');
+
+        Player opponent;
         if (args.length > 1) {
-            HumanPlayer opponent = new HumanPlayer(args[1], 'O');
-            game = new Game(HumanPlayer, opponent, boardSize);
+            opponent = new HumanPlayer(args[1], 'O');
+        } else {
+            opponent = new AIPlayer();
         }
+
+        Game game = new Game(player, opponent, boardSize);
 
         ConsolePresenter consolePresenter = new ConsolePresenter(System.out::print, new Scanner(System.in)::nextLine);
         game.run(consolePresenter);
